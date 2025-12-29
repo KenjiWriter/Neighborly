d4e0610804353aa4b3d456530b17223dba0da12b
Maciej Wiśniewski <maciej.wisniewski@aol.com>
2025-12-29 14:13:51 +0100

    feat: install and configure spatie permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable, \Spatie\Permission\Traits\HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -23,7 +23,13 @@ class User extends Authenticatable
         'email',
         'password',
         'locale',
+        'community_id',
     ];
+
+    public function community(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Community::class);
+    }
 
     public function units(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
